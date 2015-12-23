1c9cfa83c95dc8bac7d99a2a8eb84869867eaec1
suematsu555 <zap02570@hotmail.com>
2015-12-23 17:48:15 +0900

    122302

--- HelloWorld2.java
+++ HelloWorld2.java
@@ -2,6 +2,7 @@ class HelloWorld2{
 	public static void main(String args[]){
 		System.out.println("Hello　GitHub World2！！");
 		System.out.println("Hello　GitHub World2！！");//書き換えたよ
+		System.out.println("Hello　suematsu！！");//今日書き換えたよ
 	}
 
 }
